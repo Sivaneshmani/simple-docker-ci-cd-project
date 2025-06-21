3290c73b0d76e3090c2b5720c53bdf43bdb5fdce
Sivaneshmani <sivaneshmani7@gmail.com>
2025-06-21 14:10:12 +0530

    Update backend setup, Dockerfiles, and configs

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config(); // Load environment variables
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/api', (req, res) => {
   res.send('Hello from Backend API!');
